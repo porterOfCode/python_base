e201963c2c955e3966185b2afeccf4388be0135e
RSQ <2628647178@qq.com>
2018-06-16 10:53:04 +0800

    create repository

---
+++ dealdata/csv/highs_lows.py
@@ -0,0 +1,39 @@
+import csv
+from matplotlib import pyplot as plt
+
+filename = "death_valley_2014.csv"
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)   # next获取下一行数值
+    # print(header_row)
+
+    # 获取最高气温
+    highs =[]
+    info_dict = {}
+    for rows in reader:
+        if rows[1] != "":
+            highs.append(int(rows[1]))
+            info_dict['pst'] = rows[0]
+            info_dict['maxTemperatureF'] = (int(rows[1]))
+            info_dict['minTemperatureF'] = ((rows[3]))
+            print(info_dict)
+
+    print(highs)
+
+    # 根据数据绘制图形
+    fig = plt.figure(dpi=128, figsize=(10, 6))
+    plt.plot(highs, c='red')
+
+    # 设置图形的格式
+    plt.title("Daliy high temperatures, July 2014", fontsize=24)
+    plt.xlabel("", fontsize=16)
+    plt.ylabel("Temperature (F)", fontsize=16)
+
+    # 设置刻度标记的大小
+    #  axis为影响范围，both为x&y
+    plt.tick_params(axis='both', which='major', labelsize=16)
+
+    plt.show()
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
